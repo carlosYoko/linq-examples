0d8c50c2cbf5946e0ea0f9a68820d7c0fd3472b4
Carlos Gimenez <carlosgibe@gmail.com>
2024-09-14 00:59:05 +0200

    create more operators console project

---
+++ LINQExamples.MoreOperators/Program.cs
@@ -0,0 +1,104 @@
+﻿List<Employee> empolyeeList = Data.GetEmployees();
+List<Department> departmentList = Data.GetDepartments();
+
+
+public class Employee
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public decimal AnnualSalary { get; set; }
+    public bool IsManager { get; set; }
+    public int DepartmentId { get; set; }
+}
+public class Department
+{
+    public int Id { get; set; }
+    public string ShortName { get; set; }
+    public string LongName { get; set; }
+}
+
+public static class Data
+{
+    public static List<Employee> GetEmployees()
+    {
+        List<Employee> employees = new List<Employee>();
+
+        Employee employee = new Employee()
+        {
+            Id = 1,
+            FirstName = "John",
+            LastName = "Long",
+            AnnualSalary = 60000,
+            IsManager = true,
+            DepartmentId = 1,
+        };
+        employees.Add(employee);
+
+        employee = new Employee()
+        {
+            Id = 2,
+            FirstName = "Sara",
+            LastName = "Jameson",
+            AnnualSalary = 80000,
+            IsManager = true,
+            DepartmentId = 2,
+        };
+        employees.Add(employee);
+
+        employee = new Employee()
+        {
+            Id = 3,
+            FirstName = "Bob",
+            LastName = "Marley",
+            AnnualSalary = 40000,
+            IsManager = false,
+            DepartmentId = 2,
+        };
+        employees.Add(employee);
+
+        employee = new Employee()
+        {
+            Id = 4,
+            FirstName = "Faith",
+            LastName = "Evans",
+            AnnualSalary = 60000,
+            IsManager = false,
+            DepartmentId = 3,
+        };
+        employees.Add(employee);
+
+        return employees;
+    }
+
+    public static List<Department> GetDepartments()
+    {
+        List<Department> deparments = new List<Department>();
+
+        Department deparment = new Department()
+        {
+            Id = 1,
+            ShortName = "IT",
+            LongName = "Information Tecnology"
+        };
+        deparments.Add(deparment);
+
+        deparment = new Department()
+        {
+            Id = 2,
+            ShortName = "FN",
+            LongName = "Finance"
+        };
+        deparments.Add(deparment);
+
+        deparment = new Department()
+        {
+            Id = 3,
+            ShortName = "HR",
+            LongName = "Human Recourses"
+        };
+        deparments.Add(deparment);
+
+        return deparments;
+    }
+}
